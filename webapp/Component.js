30237b1740cb36ee80eabdb700e5c8a037887199
u-Kiran05 <pkirankumar@kaartech.com>
2025-07-02 12:52:30 +0530

    minor changes

--- webapp/Component.js
+++ webapp/Component.js
@@ -19,7 +19,6 @@ sap.ui.define([
 		init: function() {
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
-
 			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
 			this.getRouter().initialize();
