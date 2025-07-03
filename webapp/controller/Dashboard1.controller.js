88b05c0366adc0496077db9eaaeb9b6e7e593db8
u-Kiran05 <pkirankumar@kaartech.com>
2025-07-03 12:53:31 +0530

    minror

--- webapp/controller/Dashboard1.controller.js
+++ webapp/controller/Dashboard1.controller.js
@@ -91,153 +91,152 @@ sap.ui.define([
 			});
 		},
 
-	_updateBarChart: function (data) {
-    // Mapping of inspection type codes to labels
-    var typeLabels = {
-        "01": "Goods Receipt",
-        "02": "In-Process",
-        "03": "Final Inspection",
-        "04": "Production",
-        "05": "Audit",
-        "": "Unknown"
-    };
+		_updateBarChart: function(data) {
+			// Mapping of inspection type codes to labels
+			var typeLabels = {
+				"01": "Goods Receipt",
+				"02": "In-Process",
+				"03": "Final Inspection",
+				"04": "Production",
+				"05": "Audit",
+				"": "Unknown"
+			};
 
-    var grouped = {};
+			var grouped = {};
 
-    for (var i = 0; i < data.length; i++) {
-        var item = data[i];
-        var code = item.inspection_type || "";
-        var type = typeLabels[code] || "Other";
+			for (var i = 0; i < data.length; i++) {
+				var item = data[i];
+				var code = item.inspection_type || "";
+				var type = typeLabels[code] || "Other";
 
-        if (!grouped[type]) {
-            grouped[type] = {
-                inspection_type: type,
-                Approved: 0,
-                Rejected: 0
-            };
-        }
+				if (!grouped[type]) {
+					grouped[type] = {
+						inspection_type: type,
+						Approved: 0,
+						Rejected: 0
+					};
+				}
 
-        if (item.usage_decision === "A") {
-            grouped[type].Approved++;
-        } else if (item.usage_decision === "R") {
-            grouped[type].Rejected++;
-        }
-    }
+				if (item.usage_decision === "A") {
+					grouped[type].Approved++;
+				} else if (item.usage_decision === "R") {
+					grouped[type].Rejected++;
+				}
+			}
 
-    var chartData = [];
-    for (var key in grouped) {
-        chartData.push(grouped[key]);
-    }
+			var chartData = [];
+			for (var key in grouped) {
+				chartData.push(grouped[key]);
+			}
 
-    var oViz = this.byId("barChart");
+			var oViz = this.byId("barChart");
 
-    oViz.setDataset(new FlattenedDataset({
-        dimensions: [{
-            name: "Inspection Type",
-            value: "{inspection_type}"
-        }],
-        measures: [{
-            name: "Approved",
-            value: "{Approved}"
-        }, {
-            name: "Rejected",
-            value: "{Rejected}"
-        }],
-        data: {
-            path: "/"
-        }
-    }));
+			oViz.setDataset(new FlattenedDataset({
+				dimensions: [{
+					name: "Inspection Type",
+					value: "{inspection_type}"
+				}],
+				measures: [{
+					name: "Approved",
+					value: "{Approved}"
+				}, {
+					name: "Rejected",
+					value: "{Rejected}"
+				}],
+				data: {
+					path: "/"
+				}
+			}));
 
-    oViz.setModel(new JSONModel(chartData));
-    oViz.setVizProperties({
-        title: {
-            text: "Usage Decision by Inspection Type",
-            visible: true
-        }
-    });
+			oViz.setModel(new JSONModel(chartData));
+			oViz.setVizProperties({
+				title: {
+					text: "Usage Decision by Inspection Type",
+					visible: true
+				}
+			});
 
-    oViz.removeAllFeeds();
-    oViz.addFeed(new FeedItem({
-        uid: "valueAxis",
-        type: "Measure",
-        values: ["Approved", "Rejected"]
-    }));
-    oViz.addFeed(new FeedItem({
-        uid: "categoryAxis",
-        type: "Dimension",
-        values: ["Inspection Type"]
-    }));
-},
+			oViz.removeAllFeeds();
+			oViz.addFeed(new FeedItem({
+				uid: "valueAxis",
+				type: "Measure",
+				values: ["Approved", "Rejected"]
+			}));
+			oViz.addFeed(new FeedItem({
+				uid: "categoryAxis",
+				type: "Dimension",
+				values: ["Inspection Type"]
+			}));
+		},
 
-_updateDonutChart: function (data) {
-    var materialDescriptions = {
-        "FG": "Finished Goods",
-        "RM": "Raw Material",
-        "SFG": "Semi-Finished Goods",
-        "PKG": "Packaging Material",
-        "SP": "Spare Parts",
-        "LAB": "Lab Material",
-        "": "Unknown"
-    };
+		_updateDonutChart: function(data) {
+			var materialDescriptions = {
+				"FG": "Finished Goods",
+				"RM": "Raw Material",
+				"SFG": "Semi-Finished Goods",
+				"PKG": "Packaging Material",
+				"SP": "Spare Parts",
+				"LAB": "Lab Material",
+				"": "Unknown"
+			};
 
-    var materialCountMap = {};
+			var materialCountMap = {};
 
-    for (var i = 0; i < data.length; i++) {
-        var matCode = data[i].material_text || "";
-        var mat = materialDescriptions[matCode] || matCode || "Other";
+			for (var i = 0; i < data.length; i++) {
+				var matCode = data[i].material_text || "";
+				var mat = materialDescriptions[matCode] || matCode || "Other";
 
-        if (!materialCountMap[mat]) {
-            materialCountMap[mat] = 0;
-        }
-        materialCountMap[mat]++;
-    }
+				if (!materialCountMap[mat]) {
+					materialCountMap[mat] = 0;
+				}
+				materialCountMap[mat]++;
+			}
 
-    var donutData = [];
-    for (var key in materialCountMap) {
-        donutData.push({
-            label: key,
-            value: materialCountMap[key]
-        });
-    }
+			var donutData = [];
+			for (var key in materialCountMap) {
+				donutData.push({
+					label: key,
+					value: materialCountMap[key]
+				});
+			}
 
-    var oViz = this.byId("donutChart");
+			var oViz = this.byId("donutChart");
 
-    oViz.setVizType("donut");
-    oViz.setDataset(new FlattenedDataset({
-        dimensions: [{
-            name: "Material Type",
-            value: "{label}"
-        }],
-        measures: [{
-            name: "Count",
-            value: "{value}"
-        }],
-        data: {
-            path: "/"
-        }
-    }));
+			oViz.setVizType("donut");
+			oViz.setDataset(new FlattenedDataset({
+				dimensions: [{
+					name: "Material Type",
+					value: "{label}"
+				}],
+				measures: [{
+					name: "Count",
+					value: "{value}"
+				}],
+				data: {
+					path: "/"
+				}
+			}));
 
-    oViz.setModel(new JSONModel(donutData));
-    oViz.setVizProperties({
-        title: {
-            text: "Lots by Material Type",
-            visible: true
-        }
-    });
+			oViz.setModel(new JSONModel(donutData));
+			oViz.setVizProperties({
+				title: {
+					text: "Lots by Material Type",
+					visible: true
+				}
+			});
 
-    oViz.removeAllFeeds();
-    oViz.addFeed(new FeedItem({
-        uid: "size",
-        type: "Measure",
-        values: ["Count"]
-    }));
-    oViz.addFeed(new FeedItem({
-        uid: "color",
-        type: "Dimension",
-        values: ["Material Type"]
-    }));
-},
-
+			oViz.removeAllFeeds();
+			oViz.addFeed(new FeedItem({
+				uid: "size",
+				type: "Measure",
+				values: ["Count"]
+			}));
+			oViz.addFeed(new FeedItem({
+				uid: "color",
+				type: "Dimension",
+				values: ["Material Type"]
+			}));
+		},
 
 		_updateLineChart: function(data) {
 			var trendMap = {};
