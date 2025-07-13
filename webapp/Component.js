60d48169f852ce9707e370d7abde6f4915bfed00
u-Kiran05 <pkirankumar@kaartech.com>
2025-07-13 11:32:34 +0530

    final

--- webapp/Component.js
+++ webapp/Component.js
@@ -9,11 +9,7 @@ sap.ui.define([
 		metadata: {
 			manifest: "json"
 		},
-		/**
-		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-		 * @public
-		 * @override
-		 */
+
 		init: function() {
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
